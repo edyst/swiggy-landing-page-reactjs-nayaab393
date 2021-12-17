3f13645ae963f35d8201004c41f072bd8150e0db
nayaab393 <nayaab393>
2021-12-17 21:07:05 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+/*import ic from './ic.ico';*/
 import './App.css';
-import Second from './components/Second';
-import Third from './components/Third';
-import Fourth from './components/Fourth';
-import First from './components/First';
-import './index.css';
+
+import {Home} from './components/Home';
+import {Advn} from './components/Advn';
+import {Store} from './components/Store';
+import {About} from './components/About';
+import {Footercity} from './components/Footercity';
+import {Social} from './components/Social';
+
 function App() {
   return (
-    < div className="main">
-   <First />
-   <Second />
-   <Third />
-   <Fourth />
+    <div className="container">
+      <Home/>
+      <Advn/>
+      <Store/>
+      <About/>
+      <Footercity/>
+      <Social/>
     </div>
   );
 }
 
 export default App;
-
--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function Button(props){
-    return(
-        <a href="#">
-            <img src={props.data.src} height={props.data.height}></img>
-        </a>
-    )
-}
-export default Button;
